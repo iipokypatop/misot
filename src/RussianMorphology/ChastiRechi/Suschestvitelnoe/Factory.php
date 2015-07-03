a326de12b3dc8689f01b11d772e98671161a1ac7
Angelina <a.belousova@mivar.ru>
2015-07-03 19:19:44 +0300

    Создание фабрики для существительных.

--- src/RussianMorphology/ChastiRechi/Suschestvitelnoe/Factory.php
+++ src/RussianMorphology/ChastiRechi/Suschestvitelnoe/Factory.php
@@ -2,6 +2,31 @@
 
 namespace RussianMorphology\ChastiRechi\Suschestvitelnoe;
 
+use Dw;
+
+use MorphAttribute;
+use Word;
+
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Chislo\Edinstvennoe;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Chislo\Mnozhestvennoe;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Naritcatelnost\ImiaNaritcatelnoe;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Naritcatelnost\ImiaSobstvennoe;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Odushevlyonnost\Neodushevlyonnoe;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Odushevlyonnost\Odushevlyonnoe;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Padeszh\Datelnij;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Padeszh\Imenitelnij;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Padeszh\Predlozshnij;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Padeszh\Roditelnij;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Padeszh\Tvoritelnij;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Padeszh\Vinitelnij;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Rod\Muzhskoi;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Rod\Srednij;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Rod\Zhenskii;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Sklonenie\Pervoe;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Sklonenie\Tretie;
+use RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Sklonenie\Vtoroe;
+
+
 /**
  * Created by PhpStorm.
  * User: p.semenyuk
@@ -10,38 +35,192 @@ namespace RussianMorphology\ChastiRechi\Suschestvitelnoe;
  */
 class Factory extends \RussianMorphology\Factory
 {
-    public function build($text)
+    /**
+     * @param Dw $dw
+     * @param Word $word
+     * @throws \Exception
+     */
+    public function build(Dw $dw, Word $word)
     {
-        /**
-         * @var $protocol AnalysisProtocol
+        $text = $dw->initial_form;
+        $chislo = new Morphology\Chislo\Base();
+        $rod = new Morphology\Rod\Base();
+        $sklonenie = new Morphology\Sklonenie\Base();
+        $naritcatelnost = new Morphology\Naritcatelnost\Base();
+        $odushevlyonnost = new Morphology\Odushevlyonnost\Base();
+        $padeszh = new Morphology\Padeszh\Base();
+
+        // проверить что dw - существительное. если нет то эксептион
+
+        /*
+         *
+        Morphology\Chislo\Base $chislo,
+        Morphology\Naritcatelnost\Base $naritcatelnost,
+        Morphology\Odushevlyonnost\Base $odushevlyonnost,
+        Morphology\Padeszh\Base $padeszh,
+        Morphology\Rod\Base $rod,
+        Morphology\Sklonenie\Base $sklonenie
          */
-        $protocol = $this->analyze($text);
-
-        $protocol->similar_word_forms; // [тучка, точка, тачка]
-
-        die("asd");
-
-        if (
-            $protocol->spell_check_status === Analyser::SPELL_ANALYSIS_OK
-            && count($protocol->similar_word_forms) === 1
-        ) {
+        if (isset($word->word) && $dw->id_word_class === NOUN_CLASS_ID) {
+            foreach ($dw->parameters as $value) {
+                /** @var $value MorphAttribute */
+                # одушевленность
+                if ($value->id_morph_attr === ANIMALITY_ID) {
+                   $this->getOdushevlennost($value);
+                }
+                # род
+                elseif ($value->id_morph_attr === GENUS_ID) {
+                   $this->getRod($value);
+                }
+                # число
+                elseif ($value->id_morph_attr === NUMBER_ID) {
+                    $this->getChislo($value);
+                }
+                # склонение
+                elseif ($value->id_morph_attr === \OldAotConstants::DECLENSION) {
+                    $this->getSklonenie($value);
+                }
+                # падеж
+                elseif ($value->id_morph_attr === CASE_ID ) {
+                    $this->getPadeszh($value);
+                }
+                # нарицательность
+                elseif ($value->id_morph_attr === \OldAotConstants::SELF_NOMINAL) {
+                    $this->getNaritcatelnost($value);
+                }
+            }
             return Base::create(
-                $protocol->text,
-                $protocol->chislo[0],
-                $protocol->naritcatelnost[0],
-                $protocol->odushevlyonnost[0],
-                $protocol->padeszh[0],
-                $protocol->rod[0],
-                $protocol->sklonenie[0]
+                $text,
+                $chislo,
+                $naritcatelnost,
+                $odushevlyonnost,
+                $padeszh,
+                $rod,
+                $sklonenie
             );
-        }
 
-        return null;
+        } else throw new \Exception('not implemented yet');
     }
 
     protected function getAnalyser()
     {
         return new Analyser;
+    }
+
+    /**
+     * @param MorphAttribute $value
+     * @return \RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Base
+     */
+    protected function getOdushevlennost(MorphAttribute $value)
+    {
+        if (current($value->id_value_attr) === ANIMALITY_ANIMATE_ID) {
+            $odushevlyonnost = new Odushevlyonnoe();
+        }
+        elseif (current($value->id_value_attr) === ANIMALITY_INANIMATE_ID) {
+            $odushevlyonnost = new Neodushevlyonnoe();
+        }
+        else $odushevlyonnost = new Morphology\Odushevlyonnost\Null;
+
+        return $odushevlyonnost;
+    }
+
+    /**
+     * @param MorphAttribute $value
+     * @return \RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Base
+     */
+    protected function getRod(MorphAttribute $value) {
+
+        if (current($value->id_value_attr) === GENUS_MASCULINE_ID) {
+            $rod = new Muzhskoi();
+        }
+        elseif (current($value->id_value_attr) === GENUS_NEUTER_ID) {
+            $rod = new Srednij();
+        }
+        elseif (current($value->id_value_attr) === GENUS_FEMININE_ID) {
+            $rod = new Zhenskii();
+        }
+        else $rod = new Morphology\Rod\Null();
+
+        return $rod;
+    }
+
+    /**
+     * @param MorphAttribute $value
+     * @return \RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Base
+     */
+    protected function getChislo(MorphAttribute $value) {
+        if (current($value->id_value_attr) === NUMBER_SINGULAR_ID) {
+            $chislo = new Edinstvennoe();
+        }
+        elseif (current($value->id_value_attr) === NUMBER_PLURAL_ID) {
+            $chislo = new Mnozhestvennoe();
+        }
+        else $chislo = new Morphology\Chislo\Null();
+
+        return $chislo;
+    }
+
+    /**
+     * @param MorphAttribute $value
+     * @return \RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Base
+     */
+    protected function getSklonenie(MorphAttribute $value) {
+        if (current($value->id_value_attr) === \OldAotConstants::DECLENSION_1) {
+            $sklonenie = new Pervoe();
+        }
+        elseif (current($value->id_value_attr) === \OldAotConstants::DECLENSION_2) {
+            $sklonenie = new Vtoroe();
+        }
+        elseif (current($value->id_value_attr) === \OldAotConstants::DECLENSION_3) {
+            $sklonenie = new Tretie();
+        }
+        else $sklonenie = new Morphology\Sklonenie\Null();
+
+        return $sklonenie;
+    }
+
+    /**
+     * @param MorphAttribute $value
+     * @return \RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Base
+     */
+    protected function getPadeszh(MorphAttribute $value) {
+        if (current($value->id_value_attr) === CASE_SUBJECTIVE_ID) {
+            $padeszh = new Imenitelnij();
+        }
+        elseif (current($value->id_value_attr) === CASE_GENITIVE_ID) {
+            $padeszh = new Roditelnij();
+        }
+        elseif (current($value->id_value_attr) === CASE_DATIVE_ID) {
+            $padeszh = new Datelnij();
+        }
+        elseif (current($value->id_value_attr) === CASE_ACCUSATIVE_ID) {
+            $padeszh = new Vinitelnij();
+        }
+        elseif (current($value->id_value_attr) === CASE_INSTRUMENTAL_ID) {
+            $padeszh = new Tvoritelnij();
+        }
+        elseif (current($value->id_value_attr) === CASE_PREPOSITIONAL_ID) {
+            $padeszh = new Predlozshnij();
+        }
+        else $padeszh = new Morphology\Padeszh\Null();
+
+        return $padeszh;
+    }
+
+    /**
+     * @param MorphAttribute $value
+     * @return \RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Base
+     */
+    protected function getNaritcatelnost(MorphAttribute $value) {
+        if (current($value->id_value_attr) === \OldAotConstants::SELF) {
+            $naritcatelnost = new ImiaNaritcatelnoe();
+        }
+        elseif (current($value->id_value_attr) === \OldAotConstants::NOMINAL) {
+            $naritcatelnost = new ImiaSobstvennoe();
+        }
+        else $naritcatelnost = new Morphology\Naritcatelnost\Null();
+
+        return $naritcatelnost;
     }
 }
 
