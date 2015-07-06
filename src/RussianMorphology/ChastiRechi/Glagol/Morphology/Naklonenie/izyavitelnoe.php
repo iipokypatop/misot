7270ff3fc71ff808656b9addb2353ad0ae2715b2
Angelina <a.belousova@mivar.ru>
2015-07-06 13:57:40 +0300

    Добавление наклонения у глагола.

---
+++ src/RussianMorphology/ChastiRechi/Glagol/Morphology/Naklonenie/izyavitelnoe.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Angelina
+ * Date: 25.06.15
+ * Time: 16:39
+ */
+
+namespace RussianMorphology\ChastiRechi\Glagol\Morphology\Naklonenie;
+
+
+class Izyavitelnoe extends \RussianMorphology\ChastiRechi\Glagol\Morphology\Base
+{
+
+}
