a15db7da223c1a42c9a99b2c093c8b0495fbd424
Angelina <a.belousova@mivar.ru>
2015-07-06 13:51:32 +0300

    Корректировка морфологических признаков существительного.

---
+++ tests/Functional/Sviaz/Rule/Preprocessors/BaseTest.php
@@ -0,0 +1,57 @@
+<?php
+namespace AotTest\Functional\Sviaz\Rule\Preprocessors;
+use Constants;
+use MivarTest\Base;
+//use RussianMorphology\ChastiRechi\Suschestvitelnoe\Factory;
+//use Sviaz\Sequence;
+//use Sviaz\SequenceMember\Word;
+
+
+/**
+ * Created by PhpStorm.
+ * User: Angelina
+ * Date: 01.07.15
+ * Time: 14:21
+ */
+class BaseTest extends Base
+{
+
+    /**
+     * @param $dw
+     * @param $word
+     * @throws \Exception
+     */
+//    public function testfirst($dw, $word)
+//    {
+//
+//        $sequence = new Sequence();
+//
+//        $member = new Word();
+//        $c = new  Constants;
+//        $c->defineConstants();
+//        $syntax_parser = new \SyntaxParserManager;
+//
+//        $syntax_parser->reg_parser->parse_text("Ежик купил машины");
+//
+//        $syntax_parser->create_dictionary_word();
+//
+//        foreach ($syntax_parser->reg_parser->get_sentences() as $sentence) {
+//            $wdw[] = $syntax_parser->create_sentence_space($sentence);
+//        }
+//
+//        var_export($wdw);
+
+//        $suchestvitelnoe = Factory::get()->build();
+//
+//        $member->setSlovo();
+//
+//        $sequence->append($member);
+//
+//
+//        var_export( $member);
+
+
+
+      //  $this->assertEquals(1, $sequence->count());
+   // }
+}
