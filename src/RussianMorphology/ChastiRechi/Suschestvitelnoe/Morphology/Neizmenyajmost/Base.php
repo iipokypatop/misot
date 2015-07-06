25e4fa3aa9626278701553d567ed7388eb45d282
Angelina <a.belousova@mivar.ru>
2015-07-06 13:56:48 +0300

    Добавление морфологического признака для существительного: неизменяемость

---
+++ src/RussianMorphology/ChastiRechi/Suschestvitelnoe/Morphology/Neizmenyajmost/Base.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Angelina
+ * Date: 03.07.15
+ * Time: 19:29
+ */
+namespace RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Neizmenyajmost;
+
+
+class Base extends \RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Base
+{
+
+}
---
+++ src/RussianMorphology/ChastiRechi/Suschestvitelnoe/Morphology/Neizmenyajmost/Izmnyajmij.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Angelina
+ * Date: 03.07.15
+ * Time: 19:28
+ */
+namespace RussianMorphology\ChastiRechi\Suschestvitelnoe\Morphology\Neizmenyajmost;
+
+class Izmnyajmij
+{
+
+}
