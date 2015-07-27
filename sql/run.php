8af0b1c88fb2ebd1e23dae05b0484e5f6f4de31d
p.semenyuk <p.semenyuk@mivar.ru>
2015-07-27 18:21:38 +0300

    рефакторинг процессора

---
+++ sql/run.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p.semenyuk
+ * Date: 20.07.2015
+ * Time: 16:05
+ */
+
+
+// php run.php connection_string
+
+class aaa
+{
+    public function __construct($connection_string)
+    {
+        dbMorph::up($connection_string);
+        dbTxt::up($connection_string);
+    }
+}
+
