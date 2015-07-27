ca686bbce77baa571d89afc2739a63bf85b0173f
p.semenyuk <p.semenyuk@mivar.ru>
2015-07-27 18:29:16 +0300

    рефакторинг процессора

---
+++ src/Sviaz/PreProcessors/Base.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p.semenyuk
+ * Date: 01.07.2015
+ * Time: 13:32
+ */
+
+namespace Aot\Sviaz\PreProcessors;
+
+
+abstract class Base
+{
+    abstract public function run(\Aot\Sviaz\Sequence $sequence);
+
+    public static function create()
+    {
+        return new static();
+    }
+}
