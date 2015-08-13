9e610e2d860a4142ccd30922f31c1f64eb7d186b
p.semenyuk <p.semenyuk@mivar.ru>
2015-08-13 16:55:24 +0300

    fix

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-
+define('PROJECT_ROOT', __DIR__);
 
 require_once __DIR__ . "/vendor/autoload.php";
 
@@ -9,6 +9,10 @@ MivarDevelopmentLoader::load();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-if(!defined('LIB_DIR')){
-    define('LIB_DIR', __DIR__ . '/../mivar_text_semantic/lib/');
+if (!defined('LIB_DIR')) {
+    if (is_dir(__DIR__ . '/../mivar_text_semantic/lib/')) {
+        define('LIB_DIR', __DIR__ . '/../mivar_text_semantic/lib/');
+    } elseif( __DIR__ . '/vendor/txt/mivar_text_semantic/lib/') {
+        define('LIB_DIR', __DIR__ . '/vendor/txt/mivar_text_semantic/lib/');
+    }
 }
