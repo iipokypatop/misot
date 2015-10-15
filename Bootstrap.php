013d2284372d37fee606f571e763ea164c3f5d95
p.semenyuk <p.semenyuk@mivar.ru>
2015-10-15 17:18:41 +0300

    добавил mivar-projects/mivar-theory-classes

--- Bootstrap.php
+++ Bootstrap.php
@@ -17,7 +17,6 @@ class MorphAttribute extends \Aot\MivarTextSemantic\MorphAttribute
 {
 }
 
-
 \Overloader\Base::load([
     'txt',
     'mivar-projects',
@@ -25,13 +24,3 @@ class MorphAttribute extends \Aot\MivarTextSemantic\MorphAttribute
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
-
-
-/*if (!defined('LIB_DIR')) {
-    if (is_dir(__DIR__ . '/../mivar_text_semantic/lib/')) {
-        define('LIB_DIR', __DIR__ . '/../mivar_text_semantic/lib/');
-    } elseif (__DIR__ . '/vendor/txt/mivar_text_semantic/lib/') {
-        define('LIB_DIR', __DIR__ . '/vendor/txt/mivar_text_semantic/lib/');
-    }
-}*/
