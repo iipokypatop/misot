f3f6aa9b5397fe9fe79477113a904dc5ebd344ab
Яковенко Иван <i.yakovenko@mivar.ru>
2015-10-22 15:11:59 +0300

    Рефакторинг

--- tests/Functional/Tools/ConvertTextIntoSlovaTest.php
+++ tests/Functional/Tools/ConvertTextIntoSlovaTest.php
@@ -25,7 +25,7 @@ class ConvertTextIntoSlovaTest extends \AotTest\AotDataStorage
 
     public function testLaunch()
     {
-        $text = "Ресовать Папа пришёл домой и лёг спать. А потом и мама пришла домой.";
+        $text = "Пичаль, кот пришёл домой. А потом и пёс.";
         $res = \Aot\Tools\ConvertTextIntoSlova::convert($text);
         foreach ($res as $sentence) {
             foreach ($sentence as $key => $value) {
@@ -38,7 +38,35 @@ class ConvertTextIntoSlovaTest extends \AotTest\AotDataStorage
 
     public function testConvert()
     {
+        $words = [
+            0 => "Пичаль",
+            1 => ",",
+            2 => "кот",
+            3 => "пришёл",
+            4 => "домой",
+            5 => ".",
+            6 => "А",
+            7 => "потом",
+            8 => "и",
+            9 => "пёс",
+            10 => "."
+        ];
+        $text = "{$words[0]}{$words[1]} {$words[2]} {$words[3]} {$words[4]}{$words[5]} {$words[6]} {$words[7]} {$words[8]} {$words[9]}{$words[10]}";
+        $res = \Aot\Tools\ConvertTextIntoSlova::convert($text);
 
+        $this->assertEquals(true, is_array($res));
+        $i = 0;
+        foreach ($res as $sentence) {
+            $this->assertInstanceOf(\Aot\Tools\SentenceContainingVariantsSlov::class, $sentence);
+            $this->assertGreaterThan(0, count($sentence));
+            foreach ($sentence as $key => $value) {
+                $this->assertEquals($words[$i], $key);
+                foreach ($value as $slovo) {
+                    $this->assertInstanceOf(\Aot\RussianMorphology\Slovo::class, $slovo);
+                }
+                $i++;
+            }
+        }
     }
 
 
