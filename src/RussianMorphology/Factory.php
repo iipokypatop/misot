d30bcee4e8d1ef0a9a692d689c27142f2091885d
Яковенко Иван <i.yakovenko@mivar.ru>
2015-11-02 10:26:16 +0300

    Рефакторинг

--- src/RussianMorphology/Factory.php
+++ src/RussianMorphology/Factory.php
@@ -100,29 +100,15 @@ abstract class Factory
         foreach ($words as $word) {
             assert(is_string($word));
         }
-        /** @var \Aot\Sviaz\SequenceMember\Word\Base[] $array_words */
-        $array_words = [];
-        /** @var \Aot\Sviaz\SequenceMember\Punctuation[] $array_words_slova */
-        $array_words_slova = [];
-
+        $result = [];
         foreach ($words as $index => $word) {
             if (preg_match(static::REGULAR_FOR_WHITE_LIST, $word)) {
-                $array_words[$index] = static::getSlova([$word])[0];
+                $result[$index] = static::getSlova([$word])[0];
             } else {
-                $array_words_slova[$index][] = \Aot\RussianSyntacsis\Punctuaciya\Factory::getInstance()->build($word);
+                $result[$index] = [\Aot\RussianSyntacsis\Punctuaciya\Factory::getInstance()->build($word)];
             }
         }
 
-        $result = [];
-        $count = count($words);
-        for ($i = 0; $i < $count; $i++) {
-            if (array_key_exists($i, $array_words)) {
-                $result[] = $array_words[$i];
-            }
-            if (array_key_exists($i, $array_words_slova)) {
-                $result[] = $array_words_slova[$i];
-            }
-        }
         return $result;
     }
 
