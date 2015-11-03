e63e9787a653b7874babfa5e1dcf2b6cf56f1953
Яковенко Иван <i.yakovenko@mivar.ru>
2015-11-03 16:37:13 +0300

    Работа над препроцессором

--- src/Sviaz/PreProcessors/HomogeneitySupposed.php
+++ src/Sviaz/PreProcessors/HomogeneitySupposed.php
@@ -20,22 +20,20 @@ class HomogeneitySupposed extends \Aot\Sviaz\PreProcessors\Base
     public function run(\Aot\Sviaz\Sequence $raw_sequence)
     {
 
-
-
         // todo сделать данный метод!
+        /*
+                $member6=$raw_sequence->offsetGet(1);
+                $member7=$raw_sequence->offsetGet(2);
+                $part3 = \Aot\Sviaz\Homogeneity\HomogeneitySupposed::create(
+                    [
+                        $member6,
+                        $member7
+                    ],
+                    $raw_sequence
+                );
+                $raw_sequence->addHypothesisSupposed($part3);
 
-        $member6=$raw_sequence->offsetGet(1);
-        $member7=$raw_sequence->offsetGet(2);
-        $part3 = \Aot\Sviaz\Homogeneity\HomogeneitySupposed::create(
-            [
-                $member6,
-                $member7
-            ],
-            $raw_sequence
-        );
-        $raw_sequence->addHypothesisSupposed($part3);
-
-
+        */
         return $raw_sequence;
     }
 
