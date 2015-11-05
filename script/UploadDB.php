d26cd16506ed178896ddddf6e0631e2de058a502
Яковенко Иван <i.yakovenko@mivar.ru>
2015-11-05 13:38:04 +0300

    Скрипт для заполнения БД

--- script/UploadDB.php
+++ script/UploadDB.php
@@ -13,42 +13,42 @@ class UploadDB extends \Aot\Script\Base
 {
     public static function run()
     {
-            # chastiRechi
-            $registry = new \Aot\RussianMorphology\ChastiRechi\ChastiRechiRegistry();
-            $registry->save();
+        # chastiRechi
+        $registry = new \Aot\RussianMorphology\ChastiRechi\ChastiRechiRegistry();
+        $registry->save();
 
-            # morphology
-            $registry = new \Aot\RussianMorphology\ChastiRechi\MorphologyRegistry();
-            $registry->save();
+        # morphology
+        $registry = new \Aot\RussianMorphology\ChastiRechi\MorphologyRegistry();
+        $registry->save();
 
-            # typeLink
-            $registry = new \Aot\Sviaz\Podchinitrelnaya\Registry();
-            $registry->save();
+        # typeLink
+        $registry = new \Aot\Sviaz\Podchinitrelnaya\Registry();
+        $registry->save();
 
-            #linkChecker
-            $registry = new \Aot\Sviaz\Rule\Checker\Registry();
-            $registry->save();
+        #linkChecker
+        $registry = new \Aot\Sviaz\Rule\Checker\Registry();
+        $registry->save();
 
-            #memberChecker
-            $registry = new \Aot\Sviaz\Rule\AssertedMember\Checker\Registry();
-            $registry->save();
+        #memberChecker
+        $registry = new \Aot\Sviaz\Rule\AssertedMember\Checker\Registry();
+        $registry->save();
 
-            #textGroup
-            $registry = new \Aot\Text\GroupIdRegistry();
-            $registry->save();
+        #textGroup
+        $registry = new \Aot\Text\GroupIdRegistry();
+        $registry->save();
 
-            #position (для Third)
-            $registry = new \Aot\Sviaz\Rule\AssertedMember\PositionRegistry();
-            $registry->save();
+        #position (для Third)
+        $registry = new \Aot\Sviaz\Rule\AssertedMember\PositionRegistry();
+        $registry->save();
 
-            #presence (для Third)
-            $registry = new \Aot\Sviaz\Rule\AssertedMember\PresenceRegistry();
-            $registry->save();
+        #presence (для Third)
+        $registry = new \Aot\Sviaz\Rule\AssertedMember\PresenceRegistry();
+        $registry->save();
 
-            #operator
-            $registry = new \Aot\Sviaz\Rule\AssertedMatching\OperatorRegistry();
-            $registry->save();
-            return true;
+        #operator
+        $registry = new \Aot\Sviaz\Rule\AssertedMatching\OperatorRegistry();
+        $registry->save();
+        return true;
     }
 }
 
