d04289d058835c4a1f0bc4aeb727bc5100957dc3
Яковенко Иван <i.yakovenko@mivar.ru>
2015-11-05 15:30:52 +0300

    Добавление ролей

--- script/UploadDB.php
+++ script/UploadDB.php
@@ -52,6 +52,10 @@ class UploadDB extends \Aot\Script\Base
         $registry = new \Aot\Sviaz\Rule\AssertedMatching\OperatorRegistry();
         $registry->save();
 
+        #role
+        $registry = new \Aot\Sviaz\Role\Registry();
+        $registry->save();
+
     }
 }
 
