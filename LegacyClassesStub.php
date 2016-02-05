6a37e992be5ca4d47b83cdf3442e27e9254d18f6
p.semenyuk <p.semenyuk@mivar.ru>
2016-02-05 11:05:08 +0300

    some refactoring

---
+++ LegacyClassesStub.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peter-local
+ * Date: 005, 05, 02, 2016
+ * Time: 10:58
+ */
+
+
+class Dw extends \Aot\MivarTextSemantic\Dw
+{
+}
+
+class PointWdw extends \Aot\MivarTextSemantic\PointWdw
+{
+}
+
+class MorphAttribute extends \Aot\MivarTextSemantic\MorphAttribute
+{
+}
