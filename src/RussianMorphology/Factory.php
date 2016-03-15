d603b10c4b05cda2231206ab19c1f0fb8a81404b
Яковенко Иван <i.yakovenko@mivar.ru>
2016-03-15 13:52:24 +0300

    Fix-1

--- src/RussianMorphology/Factory.php
+++ src/RussianMorphology/Factory.php
@@ -95,6 +95,10 @@ class Factory
             $slova[$index] = [];
             foreach ($points as $point) {
                 foreach ($factory_list as $factory) {
+                    /** @warning Решить проблему с начальной формой */
+                    if (empty($point->dw)) {
+                        continue;
+                    }
                     $slova[$index] = array_merge(
                         $slova[$index],
                         $factory->build($point->dw)
