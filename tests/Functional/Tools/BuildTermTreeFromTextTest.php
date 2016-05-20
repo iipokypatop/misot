58ae7dbf63ce08c51ff6d34d49115fbeb46bbca9
Яковенко Иван <i.yakovenko@mivar.ru>
2016-05-20 11:55:24 +0300

    Реализация уникальности по начальной форме + тест

--- tests/Functional/Tools/BuildTermTreeFromTextTest.php
+++ tests/Functional/Tools/BuildTermTreeFromTextTest.php
@@ -9,6 +9,8 @@
 namespace AotTest\Functional\Sviaz\Podchinitrelnaya\Filters;
 
 
+use MivarTest\PHPUnitHelper;
+
 class BuildTermTreeFromTextTest extends \AotTest\AotDataStorage
 {
 
@@ -20,5 +22,52 @@ class BuildTermTreeFromTextTest extends \AotTest\AotDataStorage
         //print_r($res);
     }
 
+    public function testInitialFormAlreadyMet()
+    {
+        $slova = [];
+        $initial_forms = [
+            'test_1',
+            'test_2',
+            'test_2',
+            'test_3',
+            'test_4',
+            'test_4',
+            'test_5',
+        ];
+        $expected_results = [
+            false,
+            false,
+            true,
+            false,
+            false,
+            true,
+            false,
+        ];
 
+        for ($i = 0; $i <= 6; $i++) {
+            $slovo = $this->getMockBuilder(\Aot\RussianMorphology\Slovo::class)
+                ->disableOriginalConstructor()
+                ->setMethods(['_'])
+                ->getMock();
+            PHPUnitHelper::setProtectedProperty($slovo, 'initial_form', $initial_forms[$i]);
+            $slova[] = $slovo;
+        }
+
+
+        $tested_class = $this->getMockBuilder(\Aot\Tools\BuildTermTreeFromText::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $actual_results = [];
+        foreach ($initial_forms as $index => $initial_form) {
+            $actual_results[] = PHPUnitHelper::callProtectedMethod(
+                $tested_class,
+                'initialFormAlreadyMet',
+                [$slova, $index, $initial_form]
+            );
+        }
+
+        $this->assertEquals($expected_results, $actual_results);
+
+    }
 }
